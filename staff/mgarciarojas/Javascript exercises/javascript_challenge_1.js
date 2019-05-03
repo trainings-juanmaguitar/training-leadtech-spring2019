1c05115d13f366596459857ef5d25a215d82ed8b
martibiri <martagrojas@gmail.com>
2019-05-03 17:56:42 -0400

    vowel exercise

--- staff/mgarciarojas/Javascript exercises/javascript_challenge_1.js
+++ staff/mgarciarojas/Javascript exercises/javascript_challenge_1.js
@@ -2,7 +2,7 @@
 //Write a function in Javascript that having two integers x1 and x2 returns all the integers between them.
 // If x2 es lower than x1 it should return -1
 
-function NumbersBetween (a, b){
+function numbersBetween (a, b){
     if (a < b) {
         for (i=a+1; i<b; i++) {
             console.log(i)
@@ -16,7 +16,7 @@ function NumbersBetween (a, b){
 //Multiplications table
 //Write a function that writes in the console the multiplication table (from 1 to 10)
 
-function MultiplicationTable (){
+function multiplicationTable (){
     var numero;
     var tabla="";
     for (i=1; i<=10;i++) {
@@ -32,7 +32,7 @@ function MultiplicationTable (){
 // Multiplications table on demand
 //Write a function that writes in the console the multiplication table (in one column) of any number passed as parameter
 
-function MultiplicationTableOnDemand(RequestedNumber){
+function multiplicationTableOnDemand(RequestedNumber){
  for (i=1; i<=10; i++){
      console.log(RequestedNumber*i)
  }
@@ -41,7 +41,7 @@ function MultiplicationTableOnDemand(RequestedNumber){
 //Calculation
 //Write a program that writes in the console all the multiples of 23 less than 500 and at the end writes the sum of all of them
 
-function Calculation(){
+function calculation(){
     var acc=null;
     var table=""
     for (i=0; i<500; i++){
@@ -60,7 +60,7 @@ function Calculation(){
 //Define a function max() that takes two numbers as arguments and returns
 // the largest of them. Use the if-then-else construct available in Javascript.
 
-function Max (a, b){
+function max (a, b){
 if (a > b){
 console.log(a)
 }else{
@@ -70,3 +70,28 @@ console.log(b)
 
 //maxOfThree() function
 //Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+
+function maxOfThree(a, b, c){
+    if (a>b){
+        if (a>c){
+            console.log(a)
+        }
+    }else{
+        if (b>c){
+            console.log(b)
+        }else{
+            console.log(c)
+        }
+    }
+}
+
+//isVowel() function
+//Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+
+function isVowel(letter){
+    if (letter=='a' || letter=='e'|| letter =='i' || letter == 'u' || letter == 'o'){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
