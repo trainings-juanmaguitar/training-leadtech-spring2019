dabfeb8b1d31b217cc9939ff138daac5d58a5e11
Marta Garcia <m.garcia.rojas@leadtech.com>
2019-05-09 16:28:17 +0200

    done in class

--- staff/mgarciarojas/Javascript exercises/javascript_challenge_1.js
+++ staff/mgarciarojas/Javascript exercises/javascript_challenge_1.js
@@ -2,37 +2,36 @@
 //Write a function in Javascript that having two integers x1 and x2 returns all the integers between them.
 // If x2 es lower than x1 it should return -1
 
-function NumbersBetween (a, b){
-    if (a < b) {
-        for (i=a+1; i<b; i++) {
-            console.log(i)
+function numbersBetween (a, b){
+    var rangeNetween="";
+    if (a > b) {return -1 }
+     for (var i=a; i<=b; i++) {
+        rangeNetween += i + '\t';
         }
-    }else{
-        console.log (-1)
-    }
-    
+    return rangeNetween;
 }
+
 
 //Multiplications table
 //Write a function that writes in the console the multiplication table (from 1 to 10)
 
-function MultiplicationTable (){
+function multiplicationTable (){
     var numero;
     var tabla="";
     for (i=1; i<=10;i++) {
         for (k=1; k<=10;k++){
              numero = (i*k)
-           tabla+=" "+numero
+           tabla+=numero + "\t";
         }
         tabla+="\n"
     }   
-    console.log(tabla)  
+    return tabla  
 }
 
 // Multiplications table on demand
 //Write a function that writes in the console the multiplication table (in one column) of any number passed as parameter
 
-function MultiplicationTableOnDemand(RequestedNumber){
+function multiplicationTableOnDemand(RequestedNumber){
  for (i=1; i<=10; i++){
      console.log(RequestedNumber*i)
  }
@@ -41,7 +40,7 @@ function MultiplicationTableOnDemand(RequestedNumber){
 //Calculation
 //Write a program that writes in the console all the multiples of 23 less than 500 and at the end writes the sum of all of them
 
-function Calculation(){
+function calculation(){
     var acc=null;
     var table=""
     for (i=0; i<500; i++){
@@ -60,7 +59,7 @@ function Calculation(){
 //Define a function max() that takes two numbers as arguments and returns
 // the largest of them. Use the if-then-else construct available in Javascript.
 
-function Max (a, b){
+function max (a, b){
 if (a > b){
 console.log(a)
 }else{
@@ -70,3 +69,60 @@ console.log(b)
 
 //maxOfThree() function
 //Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+
+function maxOfThree(a,b,c) {
+if (a>b){
+    if (a>c && a!=b){
+        return console.log(a)
+    }else{
+        if(a==b || a==c){
+         return console.log(a)
+        }
+        return console.log(c)
+    }
+
+} else{
+    if (b>c){
+        return console.log(b)
+    } else{
+        return console.log(b);
+    }
+    
+}
+
+}
+
+function isVowel(letter){
+    switch (letter){
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+        console.log(letter + "es una vocal");
+        break;
+        default:
+        console.log(letter + "no es una vocal");
+        break;
+    }
+  
+
+}
+//Hexadecimal
+//Writes a function that convert a hexadecimal color, "blue" for example
+// "#0000FF" in its RGB representation rgb(0,0,255)". 
+//Give the function the name getRGB() and test it with this code
+
+function getRGB(hexaColor){
+    var result;
+if (hexaColor[1]=='0'){
+ result +='rgb(0'
+}else{
+
+}
+ if (hexaColor[3]=='F'){
+
+}
+    
+}
+
