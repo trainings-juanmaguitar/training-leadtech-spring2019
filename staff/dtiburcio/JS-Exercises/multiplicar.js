1c1d50474d4c2d51fe8f5adb3350e7ca5a9f8384
David Tiburcio <dtiburcio@leadtech.com>
2019-05-13 16:49:07 +0200

    Ejercicios JS

---
+++ staff/dtiburcio/JS-Exercises/multiplicar.js
@@ -0,0 +1,133 @@
+
+/*
+Integer Numbers Range
+Write a function in Javascript that having two integers x1 and x2 returns all 
+the integers between them. If x2 es lower than x1 it should return -1
+*/
+function integer (x1,x2){
+    var rangeBetween = '';
+    if (x1 > x2) { return -1}
+    for (var i=x1;i<=x2;i++){
+        rangeBetween += i + '\t';
+    }
+    return rangeBetween;
+}
+
+/*
+Multiplications table
+Write a function that writes in the console the multiplication table (from 1 to 10)
+*/
+function tablaMultiplicar (){
+    var result = '\n';
+    for (var x=1;x<=10;x++){
+        for (var y=1;y<=10;y++){
+            result += x * y + '\t';
+        }
+        result += '\n';
+    }
+    return result;
+}
+
+/**
+Multiplications table on demand
+Write a function that writes in the console the multiplication table (in one column) of any number passed as parameter
+ */
+
+function multiplicarNumeros(x){
+    var result = '';
+    for (i=1; i<=10; i++){
+        result += x * i;
+        result += '\n';
+    }
+    return result;
+}
+
+/*
+Calculation
+Write a program that writes in the console all the multiples of 23 less than 500 and at the end writes the sum of all of them
+*/
+
+function multiplesOf23(){
+    var result = '';
+    var suma = 0;
+    
+    for (var i = 23; i < 500; i += 23){
+        result += i + '\t';
+        suma += i;
+    }
+    return "Elements: " + result + "\n" + "Suma: " + suma;
+}
+
+/*
+max() function
+Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
+*/
+function max(a,b){
+    if (a > b){
+        return a;
+    }else if (a < b) {
+        return b;
+    }
+    else return "Son iguales";
+}
+
+/**
+ * maxOfThree() function
+Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+ */
+
+function maxOfThree(a,b,c){
+    if ((a >= b)&&(a >= c)){
+        return a;
+    }else if ((b >= a)&&(b >= c)) {
+        return b;
+    }else ((c >= a)&&(c >= b))
+        return c;
+}
+
+/**
+ * isVowel() function
+Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+ */
+
+function isVowel(letra){
+    var vocales = ['a','e','i','o','u','A','E','I','O','U']
+    for (var i = 0; i <= 10; i++){
+        if (letra == vocales[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Hexadecimal
+Writes a function that convert a hexadecimal color, "blue" for example "#0000FF" in its RGB representation rgb(0,0,255)". Give the function the name getRGB() and test it with this code
+
+Hint: https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/String/split
+
+>>> var a = getRGB ('#00FF00');
+>>> a;
+“rgb(0,255,0)”;
+ */
+
+function getRGB (hexacolor){
+
+}
+
+
+ /**
+  * Hexadecimal enhanced
+Improves the previous function so besides the conversion also identifies some basic colors:
+
+Black: #000000
+White: #FFFFFF
+Red: #FF0000
+Green: #00FF00
+Blue: #0000FF
+So the result can be (for these cases):
+
+>>> var a = getRGB ('#00FF00'); 
+>>> a;
+“rgb(0,255,0)   verde”;
+  */
