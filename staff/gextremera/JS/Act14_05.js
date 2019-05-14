765ac7c3a90af6fc42d391215c88a33fc30b3aab
Guillermo Extremera <gextremera@leadtech.com>
2019-05-14 22:45:05 +0200

    add homework 1 14-05-19

--- staff/gextremera/JS/Act14_05.js
+++ staff/gextremera/JS/Act14_05.js
@@ -64,3 +64,83 @@ function setPassword(pass) {
 var password = setPassword("123456");
 
 
+
+/**
+ * translate()
+ * Escriba una función translate() que traduzca un texto a 
+ * "rövarspråket". Es decir, duplicar cada consonante y colocar 
+ * una aparición de "o" en medio. Por ejemplo, traducir 
+ * ("this is fun") debería devolver la cadena 
+ * "tothohisos isos fofunon".
+ */
+
+function translate(string){
+    var origin = string;
+    var finale = [];
+    for( var i = 0; i < origin.length; i++){        
+        if(origin[i] == ' '){
+          //console.log(" ");
+          finale.push(" ")
+        }else if (origin[i] == "a" || origin[i] == "e" || origin[i] == "i" || origin[i] == "o" || origin[i] == "u"){
+          //console.log(origin[i]);
+          finale.push(origin[i])
+        } else {
+          //console.log(origin[i] + "o" + origin[i]);
+          finale.push(origin[i] + "o" + origin[i]);
+        }
+    }
+    var string = finale.toString();
+    var translated = string.replace(/[,]/g, '');
+    return translated;
+}
+
+translate("this is fun");
+
+
+/**
+ * sum() & multiply()
+ * Defina una función sum() y una función multiply() que suma y 
+ * multiplica (respectivamente) todos los números en una array de 
+ * números. Por ejemplo, la suma ([1,2,3,4]) debe devolver 10, y 
+ * la multiplicación ([1,2,3,4]) debe devolver 24.
+ */
+ 
+ function sum(number){
+    var operatorSum = number;
+    var resultSum = 0;
+    for(var i = 0; i < operatorSum.length; i++){
+        if(i === 0){
+            resultSum = operatorSum[0];
+        } else {
+            resultSum = resultSum + operatorSum[i];    
+        }    
+    }
+    return resultSum;
+ }
+ sum([1,2,3,4]);
+
+ function multiply(number){
+    var operatorMultiply = number;
+    var resultMultiply = 0;
+    for(var i = 0; i < operatorMultiply.length; i++){
+        if(i === 0){
+            resultMultiply = operatorMultiply[0];
+        } else {
+            resultMultiply = resultMultiply * operatorMultiply[i];    
+        }    
+    }
+    return resultMultiply;
+ }
+ multiply([1,2,3,4]);
+
+
+/**
+ * reverse()
+ * Defina una función reverse() que calcula la inversión de una 
+ * cadena. Por ejemplo, reverse ("jag testar") debe devolver la 
+ * cadena "ratset gaj".
+ */
+
+ function reverse(string){
+     var
+ }
