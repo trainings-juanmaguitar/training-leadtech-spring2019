6b2b604d6e451e313696f67edfc519b0aff79540
jmarques <jmarques@leadtech.com>
2019-05-16 13:39:09 +0200

    exercises-02

--- staff/jmarques/exercises-01/javascript-challenge1.js
+++ staff/jmarques/exercises-01/javascript-challenge1.js
@@ -84,3 +84,10 @@ function isVowel(cadena) {
     }
 }
 isVowel("a");
+
+// Hexadecimal
+// Writes a function that convert a hexadecimal color, "blue" for example "#0000FF" in its RGB representation rgb(0, 0, 255)". Give the function the name getRGB() and test it with this code
+
+function getRGB(hexColor) {
+
+}
