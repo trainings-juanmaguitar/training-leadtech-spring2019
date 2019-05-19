553ffb6871dcea3f1ef364b332dacc54608a5fab
mikealzina <mikealzina@gmail.com>
2019-05-19 18:06:50 +0200

    ejercicio casi acabos martes

---
+++ staff/malzina/06-extra-exercises/getFullName.js
@@ -0,0 +1,9 @@
+/*
+getFullName
+Define a function called ​getFullName​ that receives a name and a surname and return the string 
+"Your full name is " with the name & surname concatenated
+*/
+
+function getFullName(name,surname) {
+    return "Your full name is " + name + " " + surname; 
+}
