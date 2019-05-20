0578cd050cafb013ee95fb6cdd7019d010230f25
martibiri <martagrojas@gmail.com>
2019-05-20 10:56:23 -0400

    Exercises done

---
+++ staff/mgarciarojas/Javascript exercises 6/getFullName.js
@@ -0,0 +1,11 @@
+/**
+ * getFullName
+Define a function called ​getFullName​ that receives a name and a surname and return the string
+ "Your full name is " with the name & surname concatenated
+ */
+
+ function getFullName(name, surname){
+    var sGreeting="Your full name is "+name+ " "+surname 
+    
+    return sGreeting
+ }
