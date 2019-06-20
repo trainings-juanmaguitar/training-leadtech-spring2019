81703a985d9ca03b9b730cccb1b12c4f213df740
Laura Baella <lbaella@leadtech.com>
2019-06-20 16:21:57 +0200

    More exercises

---
+++ staff/lbaella/drink-tests/src/drink.js
@@ -0,0 +1,7 @@
+function drink(age) {
+
+    if (age >= 21) return "drink whisky"
+    else if (age >= 18) return "drink beer"
+    else if (age > 14) return "drink cooke"
+    else return "drink toddy"
+}
