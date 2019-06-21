bb00ec9597e16951bbd20aa512e5bd9b68eb6b2a
Marta Garcia <m.garcia.rojas@leadtech.com>
2019-06-21 07:22:10 +0200

    finished exercise

--- staff/mgarciarojas/practice_promises/bootstrap-webpack-jquery-boilerplate/src/js/helpers.js
+++ staff/mgarciarojas/practice_promises/bootstrap-webpack-jquery-boilerplate/src/js/helpers.js
@@ -1,7 +1,10 @@
 const getDataFromResponse = ({ data }) => data;
 const getFirstTenItems = data => data.slice(0, 10);
 const getItemsList = firstTenPosts =>
-  firstTenPosts.map(({ name, email }) => `<li>${name} -${email} </li>`);
+  firstTenPosts.map(
+    ({ name, email }, i) =>
+      `<li> Num.${i + 1} Name is: ${name} - E-mail is: ${email} </li>`
+  );
 const getItemsListString = firstTenItemsList => firstTenItemsList.join('');
 const writeHtmlListPosts = htmlListFirstTenPosts => {
   $('#list_posts').html(htmlListFirstTenPosts);
