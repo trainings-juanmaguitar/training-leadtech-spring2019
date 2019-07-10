84b7886219b9171e1f0d20cc852e37a473df5de2
martibiri <martagrojas@gmail.com>
2019-07-10 14:31:31 -0400

    first FAILED attempt :(

---
+++ staff/mgarciarojas/api-movies/src/helpers/index.js
@@ -0,0 +1,6 @@
+const getDataFromResponse = ({ data }) => data;
+const getResultsFromData = ({ results }) => results;
+ const getNameFromResults = results => results.map(({name}) => name);
+
+
+export { getDataFromResponse, getResultsFromData,getNameFromResults };
