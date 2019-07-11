6739c820edcb244f772ecefd90daed01c71eafcc
Guillermo Extremera <gextremera@leadtech.com>
2019-07-11 15:08:07 +0200

    add actividad peliculas

---
+++ staff/gextremera/api-film/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import FilmList from './FilmList'
+
+const App = () => (
+    <div>
+        <h1>Listado de películas</h1>
+        <p>Añade la palábra clave que quieres que contenga tu película.</p>
+        <FilmList />
+    </div>
+)
+
+export default App
